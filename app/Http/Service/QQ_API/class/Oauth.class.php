7236a386158f55431496a819a158e6f0cb78824f
zengzhonggang <zengzhonggang@kf5.com>
2020-03-20 02:38:15 +0800

    fix

--- app/Http/Service/QQ_API/class/Oauth.class.php
+++ app/Http/Service/QQ_API/class/Oauth.class.php
@@ -62,15 +62,13 @@ class Oauth{
         $keysArr = array(
             "grant_type" => "authorization_code",
             "client_id" => $this->recorder->readInc("appid"),
-            "redirect_uri" => urlencode($this->recorder->readInc("callback")),
+            "redirect_uri" => $this->recorder->readInc("callback"),
             "client_secret" => $this->recorder->readInc("appkey"),
             "code" => $_GET['code']
         );
         //------构造请求access_token的url
         $token_url = $this->urlUtils->combineURL(self::GET_ACCESS_TOKEN_URL, $keysArr);
         $response = $this->urlUtils->get_contents($token_url);
-
-        var_dump($response);die;
 
         if(strpos($response, "callback") !== false){
 
